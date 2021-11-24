315566cd49760e7f2359945a407a7ab475b3e457
Amani Louati <alouati@wissal-sys.com>
2021-11-24 06:12:48 +0100

    -

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,33 +8,12 @@ import{ContactService} from '../services/contact.service';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  contacts:contact[];
+  contacts: contact[];
   contact = new contact();
   contactForm: FormGroup;
 
   submitted = false;
-  constructor(private formBuilder: FormBuilder,private contactService:ContactService) { }
-
-  onSave(){
-    if (this.contactForm.invalid){
-      return;
-      }else{
-    this.contactService.saveContact(this.contactForm.value)
-    .subscribe(data => {
-      console.log(data);
-      alert('Message Envoyé ');
-    })
-  }
-}
-
-    removeValidators(){
-    for (const key in (this.contactForm.controls) ) {
-    this.contactForm.clearValidators();
-    this.contactForm.updateValueAndValidity();
-    } 
-
-
-  }
+  constructor(private formBuilder: FormBuilder, private contactService: ContactService) { }
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
@@ -43,8 +22,27 @@ export class FooterComponent implements OnInit {
 
       message: ['', Validators.required],
 
-  });
+    });
   }
+  onSave(){
+    if (this.contactForm.invalid){
+      return;
+      }else{
+    this.contactService.saveContact(this.contactForm.value)
+    .subscribe(data => {
+      console.log(data);
+      alert('Message Envoyé ');
+    });
+  }
+}
+
+    removeValidators(){
+      for (const key in (this.contactForm.controls) ) {
+      this.contactForm.clearValidators();
+      this.contactForm.updateValueAndValidity();
+      }
+  }
+
 
   get f() { return this.contactForm.controls ; }
 
